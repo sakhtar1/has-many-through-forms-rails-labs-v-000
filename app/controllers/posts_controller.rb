4626f5a952e9698365d418c045179e1c399e56c5
Sharmina Akhtar <sakht318@gmail.com>
2018-04-23 13:52:25 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
+    @user_uniq = @post.user_uniq
   end
 
   def index
@@ -9,11 +10,13 @@ class PostsController < ApplicationController
 
   def new
     @post = Post.new
+    @post.categories.build
+    @post.categories.build
   end
 
   def create
-    post = Post.create(post_params)
-    redirect_to post
+    @post = Post.create(post_params)
+    redirect_to post_path(@post)
   end
 
   private
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,13 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user
+
+  def user_attributes=(user_attributes)
+     user_attributes.values.each do |user_attribute|
+       user = User.find_or_create_by(user_attribute)
+       self.users << user
+     end
+   end
 
 end
