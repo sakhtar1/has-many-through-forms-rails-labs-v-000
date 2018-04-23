29fcbb990f68bf86abe9165645f4134546b0b364
Sharmina Akhtar <sakht318@gmail.com>
2018-04-23 16:05:39 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
+    @user_uniq = @post.user_uniq
   end
 
   def index
@@ -8,12 +9,16 @@ class PostsController < ApplicationController
   end
 
   def new
+
     @post = Post.new
+    @post.categories.build
+    @post.categories.build
   end
 
   def create
-    post = Post.create(post_params)
-    redirect_to post
+  
+    @post = Post.create(post_params)
+    redirect_to post_path(@post)
   end
 
   private
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,16 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :categories
 
+  def categories_attributes=(category_attributes)
+     category_attributes.values.each do |category_attribute|
+       category = Category.find_or_create_by(category_attribute)
+       self.categories << category
+     end
+   end
 
+  def user_uniq
+    self.comments.collect{|comment|comment.user}.uniq
+  end
 end
